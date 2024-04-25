1c19f7c450fb6f57365df68baff301723098890b
kai <40947038S@ntnu.edu.tw>
2024-04-25 19:14:33 +0800

    rename

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ from torchvision.transforms import v2
 
 from CorrelationDataset import CorrelationDataset
 from Trainer import Trainer
-from utils import get_model
+from models import get_model
 
 
 def parse_args():
